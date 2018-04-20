a3a9c1b40425a7ece7f4eba7ad87ec0ed1b0a8cf
Tom Davidson <tom@davidson.me.uk>
2018-04-20 22:31:42 +0100

    jest coverage checking

---
+++ jest.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  testMatch: ["**/__tests__/**/*.js?(x)", "**/?(*.)(spec|test).js?(x)"],
+  coverageThreshold: {
+    global: {
+      branches: 80,
+      functions: 80,
+      lines: 80,
+      statements: -10
+    }
+  }
+};
